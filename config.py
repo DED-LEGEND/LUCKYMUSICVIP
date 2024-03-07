4e20d478fc8ee02a2ded74a9b1f4f644c1a05592
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-03-07 14:45:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ITS-LUCKY-BOY/VIP-MUSIC",
+    "https://github.com/DED-LEGEND/LUCKYMUSICVIP",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
